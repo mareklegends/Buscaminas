60cd837498e4697f4e3b7d7d745d8072ec75f1b6
marek <marek@DESKTOP-C6IHBC3>
2019-04-04 11:12:44 +0200

    Esta acabado¿?

--- Buscaminas/src/buscaminas/Juego.java
+++ Buscaminas/src/buscaminas/Juego.java
@@ -62,25 +62,70 @@ public class Juego {
         configurarJuego();
         iniciarJuego();
         
-        boolean bandera = false;
-        
-        while (bandera!=true) {            
-        
-        switch(elegirOperacion()){
-            case 1:
-                System.out.println("Descubirir casilla");
-                System.out.println("______________________________________");                
-                
-                break;
-            case 2:
-                System.out.println("Descubrir blanco");
-                System.out.println("______________________________________");                
-                break;
-                   
+    boolean repeticion = true;
+        mostrarTablero();
+        System.out.println("");
+        while (repeticion == true) {
+            Scanner leer = new Scanner(System.in);
+            int fila = 0;
+            int columna = 0;
+            int numero = elegirOperacion();
+            switch (numero) {
+                case 1:
+
+                    System.out.print(" - Fila:");
+                    fila = leer.nextInt();
+                    System.out.print(" - Columna:");
+                    columna = leer.nextInt();
+                    if (descubrirCasilla(fila, columna) == 1) {
+                        acabarjuegoMina();
+                        repeticion = false;
+                    }
+                    if (descubrirCasilla(fila, columna) == 2) {
+                        descubrirBlanco(fila, columna);
+                        mostrarTablero();
+                    }
+                    if (descubrirCasilla(fila, columna) == 3) {
+                        System.out.println("No has indicado una posición válida.");
+                        mostrarTablero();
+                    }
+                    if (PartidaGanada()) {
+                        repeticion = false;
+                    }
+                    break;
+                case 2:
+                    System.out.print(" >>> Fila:");
+                    fila = leer.nextInt();
+                    System.out.print(" >>> Columna:");
+                    columna = leer.nextInt();
+                    if (cordenadasCorrectas(fila, columna) == true) {
+                        tablero.getCasilla(fila, columna).setVisible(true);
+                        tablero.getCasilla(fila, columna).setBandera(true);
+                        if (PartidaGanada()) {
+                            repeticion = false;
+                        }
+                    } else {
+                        System.out.println("No has indicado una posición válida.");
+                    }
+                    break;
+                case 3:
+                    System.out.print("   -> Fila:");
+                    fila = leer.nextInt();
+                    System.out.print("   -> Columna:");
+                    columna = leer.nextInt();
+                    if (cordenadasCorrectas(fila, columna) == true && tablero.getCasilla(fila, columna).isBandera()) {
+                        tablero.getCasilla(fila, columna).setBandera(false);
+                        tablero.getCasilla(fila, columna).setVisible(false);
+                    } else {
+                        System.out.println("No has indicado una posición válida.");
+                    }
+                    break;
+                case 4:
+                    acabarjuegoMina();
+                    repeticion = false;
+                    break;
+            }
         }
-            
-       }
-        
         
         
     }
@@ -92,60 +137,77 @@ public class Juego {
     
     private int elegirOperacion(){
         
-        Scanner leer = new Scanner(System.in);
-        
-        int opc=7;
-        
-        System.out.println(">>>>>>-----");
-        System.out.println("1. Descubirir casilla");
-        System.out.println("2. Descubrir blanco");
-        System.out.println("-----<<<<<<");
-        
-        while(opc!=1 || opc!=2){
-        System.out.println("Dime una opción");
-        opc=leer.nextInt();
-        System.out.println("______________________________________");
+        Scanner op = new Scanner(System.in);
+        int opc = 0;
+
+        while (opc <= 0 || opc > 4) {
+            System.out.println("-------------------------------");
+            System.out.println("  1 - Descubrir casilla");
+            System.out.println("  2 - Poner bandera");
+            System.out.println("  3 - Quitar bendera");
+            System.out.println("  4 - Retirarse");
+            System.out.println("-------------------------------");
+            System.out.print("  -> ");
+            opc = op.nextInt();
         }
-        
+
         return opc;
     } 
     private boolean cordenadasCorrectas(int fila, int columna){
         boolean acierto = false;
-        for (int i = 0; i < numFilas; i++) {
-            for (int j = 0; j < numColumnas; j++) {
-                if (fila>=0 && columna>=0) {
-                    if (fila <= numFilas && columna <= numColumnas) {
-                        acierto=true;
-                    }
-                }
-            }
+        if (fila >= 0 && fila < numFilas && columna >= 0 && columna < numColumnas && !tablero.getCasilla(fila, columna).isVisible()) {
+            acierto = true;
         }
-        
         return acierto;
     }
     
     private void acabarjuegoMina(){        
         
-        System.out.println("Juego finalizado has pisado una mina");
-        
-        //falta imprimir la solucion
+        System.out.println("Juego finalizado has pisado una mina. \nSolución de la partida:");
+        tablero.calcularTablero();
     }
     
-    private boolean descubrirCasilla(int fila, int columna){
-         boolean acierto = false;
-        
-        return acierto;
+    private int descubrirCasilla(int fila, int columna){
+           if (cordenadasCorrectas(fila, columna) == true) {
+            if (tablero.getCasilla(fila, columna).isMina()) {
+                return 1;
+            } else {
+                return 2;
+            }
+        } else {
+            return 3;
+        }
     }
     
-    private boolean descubrirBlanco(int fila, int columna){
-         boolean acierto = false;
-        
-        return acierto;
+    private void descubrirBlanco(int fila, int columna){
+          tablero.getCasilla(fila, columna).setVisible(true);
+        if (tablero.calculaNumMinasCasilla(fila, columna) == 0) {
+           tablero.getCasilla(fila, columna).setBlanco(true);
+            for (int i = (fila - 1); i < (fila + 1); i++) {
+                for (int j = (columna - 1); j < (columna + 1); j++) {
+                    if (cordenadasCorrectas(i, j)) {
+                        descubrirBlanco(i, j);
+                    }
+                }
+            }
+        }
+    
     }
     private boolean PartidaGanada(){
-          boolean fin = false;
-        
-        return fin;
+           int contador = 0;
+        int total = tablero.getNumFilas() * tablero.getNumColumnas();
+        for (int i = 0; i < tablero.getTabla().length; i++) {
+            for (int j = 0; j < tablero.getTabla()[i].length; j++) {
+                if (tablero.getCasilla(i, j).isVisible()) {
+                    contador++;
+                }
+            }
+        }
+
+        if (contador == total) {
+            return true;
+        }
+        return false;
     }
     
 }
